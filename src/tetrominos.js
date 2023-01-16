c405cc9259ece40ff63a378e486653d218c0fa56
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-01-16 11:49:29 -0700

    Changed colors to coffee pallete and changed shape of TETROMINO[T]

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -6,42 +6,42 @@ export const TETROMINOS = {
             [0, 'I', 0, 0],
             [0, 'I', 0, 0],
             [0, 'I', 0, 0]
-        ], color: '0, 255, 255'}, // aqua color
+        ], color: '149, 7, 20'}, // cranberry
     J: {
         shape: [
             [0, 'J', 0],
             [0, 'J', 0],
             ['J', 'J', 0]
-        ], color: '25, 25, 112'}, // midnite blue
+        ], color: '41, 45, 5'}, // dark green
     L: {
         shape: [
             [0, 'L', 0],
             [0, 'L', 0],
             [0, 'L', 'L']
-        ], color: '149, 7, 20'}, // cranberry
+        ], color: '178, 90, 32'}, // rust
     Z: {
         shape: [
             [0, 'Z', 'Z'],
             [0, 'Z', 0],
             ['Z', 'Z', 0]
-        ], color: '57, 255, 20'}, // neon green
+        ], color: '59, 39, 29'}, // chocolate brown
     T: {
         shape: [
             ['T', 'T', 'T'],
             [0, 'T', 0],
-            [0, 'T', 0]
-        ], color: '237, 145, 33'}, // carrot orange
+            [0, 0, 0]
+        ], color: '174, 115, 73'}, // puke color
     O: {
         shape: [
             ['O', 'O'],
             ['O', 'O']
-        ], color: '48, 25, 52'}, // dark purple
+        ], color: '200, 147, 79'}, // caramel
     S: {
         shape: [
             ['S', 'S', 0],
             [0, 'S', 0],
             [0, 'S', 'S']
-        ], color: '255, 244, 79'} // yellow
+        ], color: '232, 138, 30'} // tangerine
 }
 
 // export const randomTetromino = () => {
