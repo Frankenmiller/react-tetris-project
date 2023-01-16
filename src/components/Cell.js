255c01dbb73945b9db6d987f1464b2b6119597d2
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-01-15 19:03:26 -0700

    Project matches test of project at 51:13 of tutorial

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,7 @@ import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell type={'Z'} color={TETROMINOS['Z'].color} >Cell</StyledCell>
+    <StyledCell type={'Z'} color={TETROMINOS['Z'].color} ></StyledCell>
 )
 
 export default Cell;
--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 import bgImage from "../../images/bg.png";
 
 export const StyledTetrisWrapper = styled.div`
-width: 100vw;
-height: 100vh;
-background: url(${bgImage}) #000;
-background-size: cover;
-overflow: hidden;
+    width: 100vw;
+    height: 100vh;
+    background: url(${bgImage}) #000;
+    background-size: cover;
+    overflow: hidden;
 `
 
 export const StyledTetris = styled.div`
@@ -15,7 +15,7 @@ export const StyledTetris = styled.div`
     align-items: flex-start;
     padding: 40px;
     margin: 0 auto;
-    max-width: 900;
+    max-width: 900px;
 
     aside {
         width: 100%;
